57db7bd0a7603a149e92471debed47432bb94ee9
Elisha Lee <esl2131@columbia.edu>
2014-02-26 22:54:30 -0500

    started lab4 and created makefile

---
+++ part1/mdb-lookup.c
@@ -0,0 +1,17 @@
+/*
+ * Name: Elisha Lee
+ * UNI: esl2131
+ * File: mdb-lookup.c
+ * Description: 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "mdb.h"
+#include "mylist.h"
+
+int main(int argc, char **arv)
+{
+    return 0;
+}
