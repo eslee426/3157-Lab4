5f0f361f66e66b8d37e1b8bd508a522fab7dc8f3
Elisha Lee <esl2131@columbia.edu>
2014-03-09 22:37:16 -0400

    edited errors

--- part1/mdb.c
+++ part1/mdb.c
@@ -1,10 +1,13 @@
 #include "mdb.h"
 #include "mylist.h"
-#include <stdio.h>
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
 
 int loadmdb(FILE *fp, struct List *dest) {
     struct MdbRec myRec;
     struct Node *node = NULL;
+    int size;
     
     // puts record into linked list memory
     while (fread(&myRec, sizeof(myRec), 1, fp) == 1) {
@@ -16,23 +19,24 @@ int loadmdb(FILE *fp, struct List *dest) {
 
 
         memcpy(record, &myRec, sizeof(myRec));
-        node = addAfter(&dest, node, record);
+        node = addAfter(dest, node, record);
         if (node == NULL) {
             perror("adding error");
             exit(1); 
         }
+        size = sizeof(dest)/sizeof(record);
     }
 
     if (ferror(fp)) {
-        perror(filename);
+        perror("error reading file");
         exit(1);
     }
 
-    return (int) sizeof(dest)/sizeof(record);
+    return size;
 }
 
 
 void freemdb(struct List *list) {
-    traverseList(&list, &free);
-    removeAllNodes(&list);
+    traverseList(list, &free);
+    removeAllNodes(list);
 }
